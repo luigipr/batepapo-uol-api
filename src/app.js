4e1b462cede4cc2469996b24e117e646804fe83a
luigipr <luigiprofeti@gmail.com>
2023-06-28 11:55:21 -0300

    versão 1 - com post e get participants

---
+++ src/app.js
@@ -0,0 +1,78 @@
+import  express  from "express";
+import cors from 'cors';
+import dayjs from "dayjs";
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv"
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+dotenv.config();
+
+const mongoClient = new MongoClient(process.env.DATABASE_URL);
+let db;
+
+mongoClient.connect()
+ .then(() => db = mongoClient.db("batePapoUol"))
+ .catch((err) => console.log(err.message));
+
+
+app.post("/participants", async (req, res) => {
+    const name = req.body.name
+    if(!name) return res.sendStatus(422)
+    //salvar participante na collection de participantes
+    let time = (dayjs().format('HH:mm:ss'))
+    const message = { 
+        from: name,
+        to: 'Todos',
+        text: 'entra na sala...',
+        type: 'status',
+        time: time
+}
+    try {
+        const res = await db.collection("participants").findOne({name});
+        if (res) { return sendStatus(422)}
+        //salvar no mongodb na coleção de participantes
+        await db.collection("participants").insertOne({name})
+        //salvar a mensagem de entrar na sala
+        await db.collection("messages").insertOne(message)
+        return sendStatus(201)
+    } catch (err) {
+        return res.sendStatus(404)
+    }
+})
+
+app.get("/participants", async (req, res) => {    
+    try {   
+        const participants = db.collection("participants").find({}).toArray;
+        res.status(200).send(participants)
+    } catch (err) {
+        res.status(500)
+    }
+})
+
+app.post("/messages", async (req , res) => {
+    const {to, text, type} = req.body
+    const from = req.headers.user
+
+    if (!to || !text || type !== 'message' || type !== 'private_message' || !from ){
+        return res.sendStatus(422)
+    }
+
+    const time = (dayjs().locale('pt-br').format('HH:mm:ss'));
+
+    const message = {
+        from, to, text, type, time
+    }
+
+})
+
+app.get("/messages" , (req,res) => {
+    const user = req.headers.user;
+
+})
+
+
+
+const PORT = 5000
+app.listen(PORT, console.log(`Servidor rodando na porta ${PORT}`))
